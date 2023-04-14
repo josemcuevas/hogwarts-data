961e2a9bc97d9bec5ced8b9f15f9246c43b5380f
josemcuevas <cuevas.ci97@gmail.com>
2023-04-14 08:20:52 +0200

    File structure created, dependencies added and hilt initialized

--- app/src/main/java/com/example/hogwartsdata/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/hogwartsdata/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hogwartsdata
+package com.example.hogwartsdata.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hogwartsdata.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
