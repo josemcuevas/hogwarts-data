c3dcbf6fcb6c875ebd8a631256f31e1de85bb2e3
josemcuevas <cuevas.ci97@gmail.com>
2023-04-14 08:51:16 +0200

    DI: NetworkModule implemented

---
+++ app/src/main/java/com/example/hogwartsdata/di/NetworkModule.kt
@@ -0,0 +1,30 @@
+package com.example.hogwartsdata.di
+
+import com.example.hogwartsdata.data.network.HouseApiClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://wizard-world-api.herokuapp.com/Houses/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideHouseApiService(retrofit: Retrofit): HouseApiClient {
+        return retrofit.create(HouseApiClient::class.java)
+    }
+}
