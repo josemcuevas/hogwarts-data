f16a7dda77906afa39a572471bfb874349cf5e2e
josemcuevas <cuevas.ci97@gmail.com>
2023-04-16 19:21:49 +0200

    LAYOUT: LogOut layout started

--- app/src/main/java/com/example/hogwartsdata/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/hogwartsdata/ui/view/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.hogwartsdata.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.hogwartsdata.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.logOutButton -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
