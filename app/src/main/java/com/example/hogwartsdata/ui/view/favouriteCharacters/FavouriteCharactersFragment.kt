66e4131c03f810d6a590360d49303392c4a4c0c4
josemcuevas <cuevas.ci97@gmail.com>
2023-04-16 17:53:02 +0200

    VIEW(FavouriteCharacters): view implemented

--- app/src/main/java/com/example/hogwartsdata/ui/view/favouriteCharacters/FavouriteCharactersFragment.kt
+++ app/src/main/java/com/example/hogwartsdata/ui/view/favouriteCharacters/FavouriteCharactersFragment.kt
@@ -1,11 +1,15 @@
 package com.example.hogwartsdata.ui.view.favouriteCharacters
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hogwartsdata.R
 import com.example.hogwartsdata.data.model.HeadModel
@@ -13,12 +17,17 @@ import com.example.hogwartsdata.data.model.TraitModel
 import com.example.hogwartsdata.databinding.FragmentFavouriteCharactersBinding
 import com.example.hogwartsdata.ui.view.favouriteCharacters.favouritesList.FavouriteListAdapter
 import com.example.hogwartsdata.ui.view.houseDetail.traitsList.TraitListAdapter
+import com.example.hogwartsdata.ui.viewModel.favouriteCharacters.FavouriteCharactersViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FavouriteCharactersFragment : Fragment() {
     private lateinit var binding: FragmentFavouriteCharactersBinding
     private lateinit var favouriteListAdapter: FavouriteListAdapter
+    private val favouriteCharactersViewModel: FavouriteCharactersViewModel by viewModels()
+    private lateinit var headList: ArrayList<HeadModel>
+    private var initialHeadList: ArrayList<HeadModel> = ArrayList()
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +35,12 @@ class FavouriteCharactersFragment : Fragment() {
     ): View {
         binding = FragmentFavouriteCharactersBinding.inflate(inflater, container, false)
         initSearchView(binding.svFavourites)
+        favouriteCharactersViewModel.onCreate()
+        headListObserver()
+        isLoadingObserver()
+        favouriteCharactersViewModel.isLoading.postValue(true)
+        favouriteCharactersViewModel.getFavourite()
+
         return binding.root
     }
 
@@ -33,22 +48,53 @@ class FavouriteCharactersFragment : Fragment() {
         search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if(!query.isNullOrEmpty()){
-                    searchFavouriteByName(query.lowercase())
+                    searchFavouriteByName(query)
+                }else{
+                    headList.clear()
+                    headList.addAll(initialHeadList)
+                    favouriteListAdapter.notifyDataSetChanged()
                 }
                 return true
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
+                if(newText==""){
+                    headList.clear()
+                    headList.addAll(initialHeadList)
+                    favouriteListAdapter.notifyDataSetChanged()
+                }
                 return true
             }
         })
     }
 
+    private fun headListObserver(){
+        favouriteCharactersViewModel.headsList.observe(viewLifecycleOwner, Observer { headListObject ->
+            if(!headListObject.isNullOrEmpty()){
+                initialHeadList.addAll(headListObject)
+                headList = headListObject
+                initFavouriteRecyclerView(headList)
+            }
+        })
+    }
+    private fun isLoadingObserver(){
+        favouriteCharactersViewModel.isLoading.observe(viewLifecycleOwner, Observer { isLoading ->
+            if(isLoading){
+                binding.progressBar.visibility = View.VISIBLE
+            }else{
+                binding.progressBar.visibility = View.GONE
+            }
+        })
+    }
     private fun searchFavouriteByName(query: String){
-
+        var filterArray: ArrayList<HeadModel> = initialHeadList
+        var newHeadList: ArrayList<HeadModel> = filterArray.filter { head -> head.firstName == query || head.lastName == query } as ArrayList<HeadModel>
+        headList.clear()
+        headList.addAll(newHeadList)
+        favouriteListAdapter.notifyDataSetChanged()
     }
 
-    private fun initFavouriteRecyclerView(favouriteList: List<HeadModel>){
+    private fun initFavouriteRecyclerView(favouriteList: ArrayList<HeadModel>){
         favouriteListAdapter = FavouriteListAdapter(favouriteList)
         binding.rvFavourites.layoutManager = LinearLayoutManager(context)
         binding.rvFavourites.adapter = favouriteListAdapter
--- app/src/main/java/com/example/hogwartsdata/ui/viewModel/favouriteCharacters/FavouriteCharactersViewModel.kt
+++ app/src/main/java/com/example/hogwartsdata/ui/viewModel/favouriteCharacters/FavouriteCharactersViewModel.kt
@@ -1,27 +1,53 @@
 package com.example.hogwartsdata.ui.viewModel.favouriteCharacters
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.hogwartsdata.data.model.HeadModel
 import com.example.hogwartsdata.data.model.HouseModel
+import com.example.hogwartsdata.domain.GetFavouriteCharacters
 import com.example.hogwartsdata.domain.GetHouse
+import com.example.hogwartsdata.domain.GetHouses
+import com.example.hogwartsdata.domain.SetFavouriteCharacters
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
+import kotlin.jvm.optionals.getOrNull
 
 @HiltViewModel
 class FavouriteCharactersViewModel @Inject constructor(
-    private val getHouse: GetHouse
+    private val setFavouriteCharacters: SetFavouriteCharacters,
+    private val getFavouriteCharacters: GetFavouriteCharacters,
+    private val getHouses: GetHouses
 ) : ViewModel() {
-    var house = MutableLiveData<HouseModel?>()
     var isLoading = MutableLiveData<Boolean>(false)
+    var headsList = MutableLiveData<ArrayList<HeadModel>>()
+    fun onCreate(){
+        val mockedCharacters: List<String> = listOf("530da97d-5a83-4ea6-bc15-790edf5b5efc", "9915c5f8-9177-4f63-bba8-d04387a404f9")
+        setFavouriteCharacters(mockedCharacters)
+    }
 
-    fun onCreate(id: String?){
-        viewModelScope.launch {
-
-            val result = id?.let { getHouse(it) }
-            house.postValue(result)
-            isLoading.postValue(false)
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun getFavourite(){
+        val characterIds: List<String> = getFavouriteCharacters()
+        var characterList: ArrayList<HeadModel> = ArrayList()
+        if(!characterIds.isEmpty()){
+            viewModelScope.launch {
+                val houses: List<HouseModel> = getHouses()
+                houses.forEach{house ->
+                    house.heads.forEach { head ->
+                        if(characterIds.stream().anyMatch { character -> character == head.id}){
+                            characterList.add(head)
+                        }
+                    }
+                }
+                headsList.postValue(characterList)
+                isLoading.postValue(false)
+            }
         }
 
     }
